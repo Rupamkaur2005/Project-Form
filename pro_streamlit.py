22f171022b8ddc4a3b19a3f332a6530a9ddeb266
Rupam <rupamsunshine44@gmail.com>
2026-08-29 01:33:17 +0530

    Add files via upload

--- pro_streamlit.py
+++ pro_streamlit.py
@@ -2,25 +2,25 @@ import streamlit as st
 import sqlite3
 from database import create_table, insert_data
 import pandas as pd
-
 create_table()
-st.title("Personal Information Form")
+st.set_page_config(page_title="Personal Information Form",page_icon="🎀",layout="centered")
+st.title("🎀Personal Information Form🎀")
 st.info("This form is created by Rupam as a fun activity and practice project as a student")
 st.write("Please fill in your information below.")
 
 with st.form("personal_form"):
- name=st.text_input("Name")
- age=st.number_input("Age",min_value=1,max_value=100,step=1)
+ name=st.text_input("👤Name")
+ age=st.number_input("🎂Age",min_value=1,max_value=100,step=1)
  gender=st.selectbox("Gender",["Select Gender","Male","Female","Other"])
- color=st.text_input("color")
- fav_food=st.text_input("Fav_Food")
- fav_subject=st.text_input("Fav_Subject")
- hobby=st.text_area("Hobby")
+ color=st.text_input("🎨color")
+ fav_food=st.text_input("🍕Fav_Food")
+ fav_subject=st.text_input("📚Fav_Subject")
+ hobby=st.text_area("🎮Hobby")
 
- submit=st.form_submit_button("Submit Form")
+ submit=st.form_submit_button("✨ Submit Form ✨")
  if submit:
    if name and color and fav_food and fav_subject and hobby:
-    st.success("Form Submitted Sucessfully!")
+    st.success("🎉Form Submitted Sucessfully!")
     insert_data(name, age, gender, color, fav_food, fav_subject, hobby)
     st.write("### Submitted Information")
     st.write("Name:",name)
@@ -30,7 +30,7 @@ with st.form("personal_form"):
     st.write("Fav_subject:",fav_subject)
     st.write("Hobby:",hobby)
    else:
-    st.error("please fill all the required fields")
+    st.error("⚠️please fill all the required fields")
    st.write("### All Submitted Data")
 
 conn = sqlite3.connect("personal_info.db")
